3fa35ba6c29062b4294d2fa248a5b975d38297a4
Blake Greeley <98073180+BlakeGreeley@users.noreply.github.com>
2022-09-05 17:33:00 -0700

    updating 9/5/22

--- server/models/link.model.js
+++ server/models/link.model.js
@@ -57,9 +57,5 @@ const LinkSchema = {
 
 };
 
-LinkSchema.path('downloadURL').validate((val) => {
-    urlRegex = /(ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-/]))?/;
-    return urlRegex.test(val);
-}, 'Invalid URL.');
 
 module.exports = mongoose.model('Link', LinkSchema);
