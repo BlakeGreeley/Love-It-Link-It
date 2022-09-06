f338fc93a67ef617093d98179043ab329d70035e
Blake Greeley <98073180+BlakeGreeley@users.noreply.github.com>
2022-09-06 09:43:56 -0700

    9/6/22

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,18 @@
-import logo from './logo.svg';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      
+      <BrowserRouter>
+        <Routes>
+          <Route></Route>
+          <Route></Route>
+          <Route></Route>
+          <Route></Route>
+          <Route></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
