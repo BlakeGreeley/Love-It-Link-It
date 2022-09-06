24678d1d9429dd04823b72cc40d99c97c3b69e90
Blake Greeley <98073180+BlakeGreeley@users.noreply.github.com>
2022-09-05 17:21:35 -0700

    updating 9/5/22

--- server/routes/link.route.js
+++ server/routes/link.route.js
@@ -2,7 +2,8 @@ const linkController = require("../controllers/link.controller");
 
 module.exports = (app) => {
     app.post("/api/link", linkController.createNewLink);
-    app.get("/api/link", linkController.getOneLink);
+    app.get("/api/link/:id", linkController.getOneLink);
     app.get("api/link", linkController.getAllLinks);
-    
+    app.put("/api/pet/:id", linkController.updateLink);
+    app.delete("/api/link/:id", linkController.deleteLink);
 }
